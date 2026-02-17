5c19b7ad4a5b36a85c2268c604efb4fa6ae2f44a
Jay Ooi <epic@epicbuzzventure.com>
2026-02-17 22:32:57 +0800

    feat: Replace MR text icon with mrr-logo.png image in navbar

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import { siteConfig } from '@/lib/config'
 import { Navbar } from './Navbar'
 import { MobileMenu } from './MobileMenu'
@@ -9,18 +10,15 @@ export function Header() {
       <div className="container mx-auto">
         <div className="flex h-16 items-center justify-between px-4">
           {/* Logo */}
-          <Link href="/" className="flex items-center space-x-2 hover:opacity-90 transition-opacity">
-            <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-primary-800 text-white font-bold text-xl">
-              MR
-            </div>
-            <div className="flex flex-col">
-              <span className="text-lg font-bold leading-tight text-primary-800">
-                {siteConfig.siteName}
-              </span>
-              <span className="text-xs text-gray-600 hidden sm:block">
-                {siteConfig.nicheLabel}
-              </span>
-            </div>
+          <Link href="/" className="flex items-center hover:opacity-90 transition-opacity">
+            <Image
+              src="/mrr-logo.png"
+              alt={siteConfig.siteName}
+              width={160}
+              height={40}
+              className="h-10 w-auto object-contain"
+              priority
+            />
           </Link>
 
           {/* Desktop Navigation */}
